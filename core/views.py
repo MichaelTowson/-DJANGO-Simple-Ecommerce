fa7a35da870b884704580c84349c52a88b64f21b
Michael Towson <MTowson@gmail.com>
2021-06-01 10:26:30 -0700

    add contact html, including crispy forms

--- core/views.py
+++ core/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from .forms import ContactForm
 
 class HomeView(generic.TemplateView):
     template_name = 'index.html'
-# Create your views here.
+
+class ContactView(generic.FormView):
+    form_class = ContactForm
+    template_name = 'contact.html'
+    
+    def get_success_url(self):
+        return reverse("contact")
